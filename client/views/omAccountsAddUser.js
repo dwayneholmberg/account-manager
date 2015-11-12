17fd51cd4489afd5e6142a7aae79d78bd44eacf4
Dwayne Holmberg <dholmberg@othermachines.com>
2015-11-12 15:43:37 -0700

    remove debugging code

--- client/views/omAccountsAddUser.js
+++ client/views/omAccountsAddUser.js
@@ -115,7 +115,7 @@ Template.omAccountsAddUser.events({
       $('#password').parent().addClass('has-success');
     }
 
-    if (hasError && 0) {
+    if (hasError) {
       $('#alert').html('<p>It looks like something wasn\'t filled out correctly. Have a look and try again.</p>');
       $('#alert').removeClass('alert-success');
       $('#alert').addClass('alert-danger');
